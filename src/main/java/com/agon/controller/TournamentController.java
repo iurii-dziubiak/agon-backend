336983729e3409a57332a81a383e0dcac3a43ebb
Iurii <ikariyuu@gmail.com>
2022-07-18 22:49:50 +0200

    Controller separated, CorsConfig added

--- src/main/java/com/agon/controller/TournamentController.java
+++ src/main/java/com/agon/controller/TournamentController.java
@@ -1,10 +1,8 @@
 package com.agon.controller;
 
 import com.agon.dto.TournamentDTO;
-import com.agon.model.Game;
 import com.agon.model.Player;
 import com.agon.model.Tournament;
-import com.agon.service.GameService;
 import com.agon.service.PlayerService;
 import com.agon.service.TournamentService;
 import lombok.RequiredArgsConstructor;
@@ -19,18 +17,10 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-public class Controller {
-    private final GameService gService;
-    private final TournamentService tService;
-    private final PlayerService pService;
+public class TournamentController {
+    private final TournamentService tournamentService;
+    private final PlayerService playerService;
 
-    @CrossOrigin(origins = "http://localhost:3000")
-    @GetMapping("/games")
-    public List<Game> getAllGames() {
-        return gService.getAll();
-    }
-
-    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/tournament")
     public ResponseEntity setNewTournament(@RequestBody TournamentDTO tDTO) {
         Tournament tournament = new Tournament();
@@ -41,14 +31,13 @@ public class Controller {
         tDTO.getPlayers().forEach(player -> {
             Player tempPlayer = new Player();
             tempPlayer.setNickName(player);
-            tempPlayer.setTournaments(new ArrayList<>());
             tempPlayer.addTournament(tournament);
-            pService.savePlayer(tempPlayer);
+            playerService.savePlayer(tempPlayer);
             players.add(tempPlayer);
         });
 
         tournament.setPlayers(players);
-        tService.saveTournament(tournament);
+        tournamentService.saveTournament(tournament);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
