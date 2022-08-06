33570c3034280de4a942c3bd720055f471622a18
Iurii <ikariyuu@gmail.com>
2022-08-06 16:58:09 +0200

    To/fromTournament, getTournament() implementation.

--- src/main/java/com/agon/controller/TournamentController.java
+++ src/main/java/com/agon/controller/TournamentController.java
@@ -1,7 +1,6 @@
 package com.agon.controller;
 
 import com.agon.dto.TournamentDTO;
-import com.agon.model.Player;
 import com.agon.model.Tournament;
 import com.agon.service.PlayerService;
 import com.agon.service.TournamentService;
@@ -11,8 +10,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestController
@@ -21,24 +21,23 @@ public class TournamentController {
     private final TournamentService tournamentService;
     private final PlayerService playerService;
 
+    @GetMapping("/tournament/{id}")
+    ResponseEntity<?> getTournament(@PathVariable Integer id) {
+        Tournament tournament = tournamentService.get(id);
+        return ResponseEntity.ok().body(TournamentDTO.fromTournament(tournament));
+    }
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoSuchElementException.class)
+    public void handleProductCreateException(NoSuchElementException e) {
+        log.error(e.getMessage());
+    }
+
     @PostMapping("/tournament")
-    public ResponseEntity setNewTournament(@RequestBody TournamentDTO tDTO) {
-        Tournament tournament = new Tournament();
-        tournament.setTitle(tDTO.getTitle());
-        tournament.setGame(tDTO.getGame());
-
-        List<Player> players = new ArrayList<>();
-        tDTO.getPlayers().forEach(player -> {
-            Player tempPlayer = new Player();
-            tempPlayer.setNickName(player);
-            tempPlayer.addTournament(tournament);
-            playerService.savePlayer(tempPlayer);
-            players.add(tempPlayer);
-        });
-
-        tournament.setPlayers(players);
+    public ResponseEntity setNewTournament(@RequestBody TournamentDTO tDTO) throws URISyntaxException {
+        Tournament tournament = tDTO.toTournament();
         tournamentService.saveTournament(tournament);
-
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        tournament.getPlayers().forEach(playerService::savePlayer);
+        tDTO.setId(tournament.getId());
+        return ResponseEntity.created(new URI("http://localhost:8088/api/tournament/"+tDTO.getId())).body(tDTO);
     }
 }
